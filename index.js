ef18db0e01c647bc2fae8028a0894cb2150b313d
Chayan Rastogi <80814459+chayanrastogi@users.noreply.github.com>
2023-05-05 00:51:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,174 +1,124 @@
-var count = 0;
-var students = [];
+let count = 0;
+let students = [];
+
+// Cache frequently used elements
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const ageInput = document.getElementById('age');
+const gradeInput = document.getElementById('grade');
+const degreeInput = document.getElementById('degree');
+const tbody = document.getElementById('tbody');
+const searchInput = document.getElementById('search');
 
 function addStudent() {
+  // Get input values
+  const nameValue = nameInput.value;
+  const emailValue = emailInput.value;
+  const ageValue = ageInput.value;
+  const gradeValue = gradeInput.value;
+  const degreeValue = degreeInput.value;
 
-    const nameValue = document.getElementById('name').value;
-    const emailValue = document.getElementById('email').value;
-    const ageValue = document.getElementById('age').value;
-    const gradeValue = document.getElementById('grade').value;
-    const degreeValue = document.getElementById('degree').value;
+  // Check for empty inputs
+  if (!nameValue || !emailValue || !ageValue || !gradeValue || !degreeValue) {
+    alert("All fields are required!");
+    return;
+  }
 
-    //Inputs Mandatory, Blank will not be submitted.
-    if (nameValue == '' || emailValue == '' || ageValue == '' || gradeValue == '' || degreeValue == "") {
-        alert("All fields are required!")
-        return;
-    }
+  // Increment ID and add student to array
+  count++;
+  students.push({ ID: count, name: nameValue, email: emailValue, age: ageValue, grade: gradeValue, degree: degreeValue });
 
-    //Iterate the ID
-    count++;
+  // Store the updated students array in local storage
+  localStorage.setItem("students", JSON.stringify(students));
 
-    students.push({
-        ID: count, name: nameValue, email: emailValue, age: ageValue, grade: gradeValue, degree: degreeValue
-    });
+  // Clear the input fields
+  nameInput.value = '';
+  emailInput.value = '';
+  ageInput.value = '';
+  gradeInput.value = '';
+  degreeInput.value = '';
 
-    // Store the updated students array in local storage
-    localStorage.setItem("students", JSON.stringify(students));
-
-    // Clear the input fields   
-    document.getElementById('name').value = "";
-    document.getElementById('email').value = "";
-    document.getElementById('age').value = "";
-    document.getElementById('grade').value = "";
-    document.getElementById('degree').value = "";
-    console.log(students);
-    showTable();
+  // Update table
+  showTable();
 }
 
 function showTable() {
-    const table = document.getElementById('tbody');
-    while (table.hasChildNodes()) {
-        table.removeChild(table.firstChild);
-    }
+  // Remove existing table rows
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
 
-    table.value = "";
-    students.forEach((student) => {
-        const row = document.createElement("tr");
+  // Create new table rows
+  students.forEach((student) => {
+    const row = document.createElement('tr');
+    row.id = `row-${student.ID}`;
 
-        var keys = Object.keys(student);
-        var id = document.createElement('td');
+    Object.keys(student).forEach((key) => {
+      const cell = document.createElement('td');
+      cell.innerHTML = student[key];
+      row.appendChild(cell);
+    });
 
-        const name = document.createElement('td');
-        const email = document.createElement('td');
-        const age = document.createElement('td');
-        const grade = document.createElement('td');
-        const degree = document.createElement('td');
+    const actionsCell = document.createElement('td');
+    const editButton = document.createElement('a');
+    editButton.classList.add('fa');
+    editButton.innerHTML = '&#xf044;';
+    editButton.addEventListener('click', () => edit(student.ID));
+    const deleteButton = document.createElement('a');
+    deleteButton.classList.add('fa');
+    deleteButton.innerHTML = '&#xf1f8;';
+    deleteButton.addEventListener('click', () => del(student.ID));
+    actionsCell.appendChild(editButton);
+    actionsCell.appendChild(deleteButton);
+    row.appendChild(actionsCell);
 
-        keys.forEach((key) => {
-            if (key == 'ID') {
-                id.innerHTML = student[key];
-            }
-            else if (key == 'name') {
-                name.innerHTML = student[key];
-            }
-            else if (key == 'email') {
-                email.innerHTML = student[key];
-            }
-            else if (key == 'age') {
-                age.innerHTML = student[key];
-            }
-            else if (key == 'grade') {
-                grade.innerHTML = student[key];
-            }
-            else{
-                degree.innerHTML = `<div>${
-                    student[key]
-                }
-                </div> <div class="icons"><a onClick="edit(${student['ID']})" class='fa'>&#xf044;</a> <a onClick="del(${student['ID']})" class='fa'>&#xf1f8;</a> </div> `;
-            // degree.innerHTML = student[key] + "  <li class='fa'>&#xf044;</li>";
-            }
-                
-                 
-            row.appendChild(id);
-            row.appendChild(name);
-            row.appendChild(email);
-            row.appendChild(age);
-            row.appendChild(grade);
-            row.appendChild(degree);
-        })
-        table.appendChild(row);
-    })
+    tbody.appendChild(row);
+  });
 }
 
 function search() {
-    var input, filter, table, tr, td, i, txtValue, txtValue1, txtValue2;
-    input = document.getElementById("search");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("tbody");
-    tr = table.getElementsByTagName("tr");
-
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[1];
-        td1 = tr[i].getElementsByTagName("td")[2];
-        td2 = tr[i].getElementsByTagName("td")[5];
-
-        if (td || td1 || td2) {
-            txtValue = td.textContent || td.innerText;
-            txtValue1 = td1.textContent || td1.innerText;
-            txtValue2 = td2.textContent || td2.innerText;
-
-            if (txtValue.toUpperCase().indexOf(filter) > -1 || txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            }
-
-            else {
-                tr[i].style.display = "none";
-            }
-
-        }
+  const filter = searchInput.value.toUpperCase();
+  students.forEach((student) => {
+    const row = document.getElementById(`row-${student.ID}`);
+    const name = student.name.toUpperCase();
+    const email = student.email.toUpperCase();
+    const degree = student.degree.toUpperCase();
+    if (name.includes(filter) || email.includes(filter) || degree.includes(filter)) {
+      row.style.display = '';
+    } else {
+      row.style.display = 'none';
     }
+  });
 }
 
-
 function edit(id) {
-    students.forEach((student) => {
-        if (student['ID'] == id) {
-            document.getElementById('name').value = student['name'];
-            document.getElementById('email').value = student['email'];
-            document.getElementById('age').value = student['age'];
-            document.getElementById('grade').value = student['grade'];
-            document.getElementById('degree').value = student['degree'];
-            document.getElementById('submit').innerText = 'Edit Student';
-
-
-            document.getElementById("submit").onclick = function jsFunc() {
-
-                student['name'] = document.getElementById('name').value;
-                student['email'] = document.getElementById('email').value;
-                student['age'] = document.getElementById('age').value;
-                student['grade'] = document.getElementById('grade').value;
-                student['degree'] = document.getElementById('degree').value;
-
-                document.getElementById('name').value = "";
-                document.getElementById('email').value = "";
-                document.getElementById('age').value = "";
-                document.getElementById('grade').value = "";
-                document.getElementById('degree').value = "";
-
-                document.getElementById('submit').innerText = 'Add Student';
-
-                showTable();
-            }
-        }
-    })
+  const student = students.find(s => s.ID === id);
+  if (!student) {
+    return;
+  }
+  nameInput.value = student.name;
+  emailInput.value = student.email;
+  ageInput.value = student.age;
+  gradeInput.value = student.grade;
+  degreeInput.value = student.degree;
 }
 
 function del(id) {
-    students.forEach((student, index) => {
-        if (student['ID'] == id) {
-            students.splice(index, 1);
-            showTable();
-        }
-    })
+  const index = students.findIndex(s => s.ID === id);
+  if (index === -1) {
+    return;
+  }
+  students.splice(index, 1);
+  localStorage.setItem("students", JSON.stringify(students));
+  showTable();
 }
 
-window.onload = () => {
-    students = JSON.parse(localStorage.getItem('students')) || [];
-    count = students.reduce((max, student) => Math.max(max, student.ID), 0);
+// Load students from local storage on page load
+const savedStudents = localStorage.getItem('students');
+if (savedStudents) {
+    students = JSON.parse(savedStudents);
+    count = students.length;
     showTable();
-};
-
-window.onbeforeunload = () => {
-    localStorage.setItem('students', JSON.stringify(students));
-};
+    } else {
+    students = [];
+    }
